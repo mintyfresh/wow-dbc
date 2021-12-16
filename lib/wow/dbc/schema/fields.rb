7b70125a732b50e6430c111c1e1e960753f75bea
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-12-15 20:40:13 -0500

    Improve error messages from schema fields.

--- lib/wow/dbc/schema/fields.rb
+++ lib/wow/dbc/schema/fields.rb
@@ -33,8 +33,17 @@ module WoW
           fields << Field.new(name.to_sym, format, index)
           @fields_count += 1
 
-          define_method(name) { instance_variable_get(:"@#{name}") }
-          define_method(:"#{name}=") { |value| instance_variable_set(:"@#{name}", format.cast(value)) }
+          define_singleton_method(:"#{name}_format") { format }
+
+          class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
+            def #{name}
+              @#{name}
+            end
+
+            def #{name}=(value)
+              @#{name} = self.class.#{name}_format.cast(value)
+            end
+          RUBY
         end
 
         # @param name [Symbol]
@@ -48,38 +57,25 @@ module WoW
             field(:"#{name}#{index}", format)
           end
 
-          define_array_reader(name, length)
-          define_array_writer(name, length)
+          class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
+            def #{name}
+              Array.new(#{length}) { |index| send(#{name.inspect} + index.to_s) }
+            end
+
+            def #{name}=(value)
+              raise ArgumentError, 'Value must be an Array.' unless value.is_a?(Array)
+              raise ArgumentError, 'Value must have exactly #{length} elements' unless value.length == #{length}
+
+              value.each_with_index do |element, index|
+                send(#{name.inspect} + index.to_s + '=', element)
+              end
+            end
+          RUBY
         end
 
         Format::MAPPINGS.each_key do |format|
           define_method(format) { |name| field(name, format) }
           define_method(:"#{format}_array") { |name, length| array(name, format, length) }
-        end
-
-      private
-
-        # @param name [Symbol]
-        # @param length [Integer]
-        # @return [void]
-        def define_array_reader(name, length)
-          define_method(name) do
-            Array.new(length) { |index| send(:"#{name}#{index}") }
-          end
-        end
-
-        # @param name [Symbol]
-        # @param length [Integer]
-        # @return [void]
-        def define_array_writer(name, length)
-          define_method(:"#{name}=") do |value|
-            raise ArgumentError, 'Value must be an Array.' unless value.is_a?(Array)
-            raise ArgumentError, "Value must have exactly #{length} elements" unless value.length == length
-
-            value.each_with_index do |element, index|
-              send(:"#{name}#{index}=", element)
-            end
-          end
         end
       end
     end
